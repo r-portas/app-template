f8e434a8faa34cd719ec75d565c2c959ab22bfb2
Roy Portas <royportas@gmail.com>
2023-01-07 09:49:25 +1000

    Add Docker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Needed for Docker
+  output: "standalone",
 };
 
 module.exports = nextConfig;
