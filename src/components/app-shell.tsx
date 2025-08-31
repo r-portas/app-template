afc91a2bd8cc02a09e226f5758f90e3febdd887c
Roy Portas <royportas@gmail.com>
2025-08-31 15:10:33 +1000

    Use custom sidebar

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -1,98 +1,34 @@
 import type { ReactNode } from "react";
-import Link from "next/link";
-import {
-  Calendar,
-  Home,
-  Inbox,
-  Search,
-  Settings,
-  type LucideIcon,
-} from "lucide-react";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarProvider,
-  SidebarInset,
-} from "@/components/ui/sidebar";
-
-interface Item {
-  title: string;
-  url: string;
-  icon: LucideIcon;
-}
+import { Home, Inbox, Settings } from "lucide-react";
+import { Sidebar, type Item } from "./ui/sidebar";
 
 // Menu items.
 const items: Item[] = [
   {
-    title: "Home",
-    url: "/",
+    label: "Home",
+    href: "/",
     icon: Home,
   },
   {
-    title: "Inbox",
-    url: "#",
+    label: "Inbox",
+    href: "#",
     icon: Inbox,
   },
   {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
+    label: "Settings",
+    href: "#",
     icon: Settings,
   },
 ];
-
-/**
- * The main sidebar for the application
- */
-function AppSidebar() {
-  return (
-    <Sidebar variant="inset">
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
-  );
-}
 
 /**
  * The main application shell, which handles the sidebar and main content area
  */
 export function AppShell({ children }: { children: ReactNode }) {
   return (
-    <SidebarProvider open={true}>
-      <AppSidebar />
-      <SidebarInset>{children}</SidebarInset>
-    </SidebarProvider>
+    <div className="flex flex-row min-h-screen">
+      <Sidebar items={items} className="grow" />
+      {children}
+    </div>
   );
 }
