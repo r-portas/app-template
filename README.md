0fc8ba27e96f9b180e3472594ec17b680c40c26a
Roy Portas <royportas@gmail.com>
2026-02-15 16:17:03 +1000

    Initial setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# App Template
+
+```bash
+# Install dependencies
+bun install
+
+# Start the development server
+bun dev
+```
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+
+const config = defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
