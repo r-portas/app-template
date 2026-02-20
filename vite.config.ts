6f49a017f6098e1c7eafbfe99ca6d72010ac5903
Roy Portas <royportas@gmail.com>
2026-02-21 06:51:34 +1000

    Add devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { devtools } from "@tanstack/devtools-vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
@@ -8,6 +9,7 @@ const config = defineConfig({
     port: 3000,
   },
   plugins: [
+    devtools(), // this plugin needs to be first
     // this is the plugin that enables path aliases
     viteTsConfigPaths(),
     tanstackStart(),
