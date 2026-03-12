96b70493a12ff71e9d62b77cb026b8ca67ddd642
Roy Portas <royportas@gmail.com>
2026-03-13 05:08:37 +1000

    Bump vite version

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,15 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    viteTsConfigPaths(), // this is the plugin that enables path aliases
-    tanstackStart(),
-    viteReact(),
-    tailwindcss(),
-  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [tanstackStart(), viteReact(), tailwindcss()],
 });
 
 export default config;
